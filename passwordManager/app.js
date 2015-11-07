3002ed09741ab9dcba879c6de4304b9ca683a957
Jennifer <jennifergapay@gmail.com>
2015-11-06 22:12:01 -0500

    Node PassMgr: Added yargs module, test

--- passwordManager/app.js
+++ passwordManager/app.js
@@ -1,1 +1,42 @@
 console.log('starting password manager');
+
+var storage = require('node-persist');
+storage.initSync();
+
+function createAccount(account) {
+  var accounts = storage.getItemSync('accounts');
+
+  if (typeof accounts === 'undefined') {
+    accounts = [];
+  }
+
+  accounts.push(account);
+  storage.setItemSync('accounts', accounts);
+
+  return account;
+
+}
+
+function getAccount(accountName) {
+  var accounts = storage.getItemSync('accounts');
+  var matchedAccount;
+
+  accounts.forEach(function(account) {
+    if (account.name === accountName) {
+      matchedAccount = account;
+    }
+  });
+
+  return matchedAccount;
+}
+
+// test //
+
+// createAccount({
+//   name: 'Twitter',
+//   username: 'jen@test.com',
+//   password: 'password123'
+// });
+
+var twitterAccount = getAccount('Twitter');
+console.log(twitterAccount);
