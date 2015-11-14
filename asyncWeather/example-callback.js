b0ade925fe12a981f7aeb7f29b1cfeca9127503a
Jennifer <jennifergapay@gmail.com>
2015-11-13 23:16:22 -0500

    Node asyncWeather: create weather.js and utilize module.exports

--- asyncWeather/example-callback.js
+++ asyncWeather/example-callback.js
@@ -1,17 +1,5 @@
-var request = require('request');
+var weather = require('./weather.js');
 
-var url = 'http://api.openweathermap.org/data/2.5/weather?q=NewYork&units=imperial&appid=2de143494c0b295cca9337e1e96b00e0';
-
-request({
-  url: url,
-  json: true
-}, function(error, response, body) {
-  if (error) {
-    console.log('Unable to fetch weather.');
-  } else {
-    // console.log(JSON.stringify(body, null, 4));
-    //It's 49.78 degrees in New York city
-    console.log('It is ' + body.main.temp + ' in ' + body.name = +'!');
-
-  }
+weather(function(currentWeather) {
+  console.log(currentWeather);
 });
