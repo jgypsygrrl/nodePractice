0e720039fdcfbbed92832fb716409b31b6cc113e
Jennifer <jennifergapay@gmail.com>
2015-11-22 23:03:16 -0500

    node asyncWeather: Replaced callbacks to promises on main app

--- asyncWeather/weather.js
+++ asyncWeather/weather.js
@@ -1,22 +1,24 @@
 var request = require('request');
 
-module.exports = function(location, callback) {
-  var encodedLocation = encodeURIComponent(location);
-  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + encodedLocation + '&units=imperial&appid=2de143494c0b295cca9337e1e96b00e0';
+module.exports = function(location) {
+  return new Promise(function(resolve, reject) {
+    var encodedLocation = encodeURIComponent(location);
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + encodedLocation + '&units=imperial&appid=2de143494c0b295cca9337e1e96b00e0';
 
-  if (!location) {
-    return callback('No location provided');
-  }
-
-  request({
-    url: url,
-    json: true
-  }, function(error, response, body) {
-    if (error) {
-      callback('Unable to fetch weather.');
-    } else {
-      callback('It\'s ' + body.main.temp + ' degrees in ' + body.name + '!');
-
+    if (!location) {
+      return reject('No location provided');
     }
-  });
+
+    request({
+      url: url,
+      json: true
+    }, function(error, response, body) {
+      if (error) {
+        reject('Unable to fetch weather.');
+      } else {
+        resolve('It\'s ' + body.main.temp + ' degrees in ' + body.name + '!');
+
+      }
+    });
+  })
 }
