729d46a2d93af8a7c56053f4fdbfde80bf436111
Jennifer <jennifergapay@gmail.com>
2015-11-23 23:07:48 -0500

    node Express:  Start Express server

--- asyncWeather/app.js
+++ asyncWeather/app.js
@@ -14,9 +14,7 @@ var argv = require('yargs')
 
 if (typeof argv.l === 'string' && argv.l.length > 0) {
   console.log('Location was provided');
-  // weather(argv.l, function(currentWeather) {
-  //   console.log(currentWeather);
-  // });
+
   weather(argv.l).then(function(currentWeather) {
     console.log(currentWeather);
   }).catch(function(error) {
@@ -24,16 +22,7 @@ if (typeof argv.l === 'string' && argv.l.length > 0) {
   })
 } else {
   console.log('Location was not provided');
-  //guess location via IP
-  // location(function(location) {
-  //   if (location) {
-  //     weather(location.city, function(currentWeather) {
-  //       console.log(currentWeather)
-  //     });
-  //   } else {
-  //     console.log('Unable to guess location')
-  //   }
-  // })
+
   location().then(function(loc) {
     return weather(loc.city);
   }).then(function(currentWeather) {
