07a3fe843ce41f0559d49c94c5aa2679a9accb5b
Jennifer <jennifergapay@gmail.com>
2015-11-24 00:32:25 -0500

    node Express:  Added timestamp middleware

--- Express-server/server.js
+++ Express-server/server.js
@@ -6,8 +6,14 @@ var middleware = {
   requireAuthentication: function(req, res, next) {
     console.log('private route hit!');
     next();
+  },
+  logger: function(req, res, next) {
+    console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+    next();
   }
 };
+
+app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function(req, res) {
   res.send('About Us');
