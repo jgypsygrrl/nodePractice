1d4d540b26b1e059bae32e017f04d97be4fb1fe2
Jennifer <jennifergapay@gmail.com>
2015-11-23 23:48:52 -0500

    node Express: revised server file to point to new public files

--- Express-server/server.js
+++ Express-server/server.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
+var PORT = 3000;
 
-app.get('/', function(req, res) {
-  res.send('Hello Express!');
+app.get('/about', function(req, res) {
+  res.send('About Us');
 });
 
-app.listen(3000);
+//tell express where to find the public folder
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function() {
+  console.log('Express server started on port ' + PORT);
+});
